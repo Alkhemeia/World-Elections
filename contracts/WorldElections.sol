58d3e35c12ebacf411a87d78496a94731d1fac9f
Alkhemeia <admin@alkhemeia.de>
2023-11-15 08:22:44 +0100

    added file

---
+++ contracts/WorldElections.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: UNKNOWN 
+pragma solidity ^0.8.9;
+
+contract Elections {
+    using ByteHasher for bytes;
+    address public owner_address;
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///                                  ERRORS                                ///
+    //////////////////////////////////////////////////////////////////////////////
+
+    /// @notice Thrown when attempting to reuse a nullifier
+    error InvalidNullifier();
+
+    /// @dev The World ID instance that will be used for verifying proofs
+    IWorldID internal immutable worldId;
+
+    /// @dev The contract's external nullifier hash
+    uint256 internal immutable externalNullifier;
+
+    /// @dev The World ID group ID (always 1)
+    uint256 internal immutable groupId = 1;
+
+    /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
+    mapping(uint256 => bool) internal nullifierHashes;
+
+    uint256 eid = 0;
+    uint256[] votes;
+    bool running = false;
+
+    /// @param _worldId The WorldID instance that will verify the proofs
+    /// @param _appId The World ID app ID
+    /// @param _actionId The World ID action ID
+    constructor(
+        IWorldID _worldId,
+        string memory _appId,
+        string memory _actionId
+    ) {
+        owner_address = msg.sender;
+        worldId = _worldId;
+        externalNullifier = abi
+            .encodePacked(abi.encodePacked(_appId).hashToField(), _actionId)
+            .hashToField();
+    }
+
+    /**
+     * @dev TODO: add description
+     */
+    function startVoting() public {
+        if (msg.sender == owner_address && !running) {
+            box[eid] = VotingBox(new address[](0), new uint256[](0));
+            running = true;
+        }
+        else {
+            revert();
+        }
+    }
+
+    /**
+     * @dev TODO: add description
+     */
+    function stopVoting() public {
+        if (msg.sender == owner_address && running) {
+            running = false;
+
+            eid++;
+        }
+        else {
+            revert();
+        }
+    }
+
+        /// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further details)
+    /// @param root The root of the Merkle tree (returned by the JS widget).
+    /// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
+    /// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
+    /// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
+    function sendVote (
+        uint256 vote,
+        address signal,
+        uint256 root,
+        uint256 nullifierHash,
+        uint256[8] calldata proof
+    ) public {
+        // First, we make sure this person hasn't done this before
+        if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
+
+        // We now verify the provided proof is valid and the user is verified by World ID
+        worldId.verifyProof(
+            root,
+            groupId,
+            abi.encodePacked(signal).hashToField(),
+            nullifierHash,
+            externalNullifier,
+            proof
+        );
+
+        // We now record the user has done this, so they can't do it again (proof of uniqueness)
+        nullifierHashes[nullifierHash] = true;
+
+        votes.push(vote);
+    }
+
+    /**
+     * @dev TODO: add description
+     */
+    function getResults(uint256 id) public view returns (uint256[] memory) {
+        return box[id].votes;
+    }
+
+}
